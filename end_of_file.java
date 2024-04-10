53d96f59ceeaac5190fa2ada6e683d9dc37f1003
Tejaswi Darisipudi <147420948+Tejaswi16072003@users.noreply.github.com>
2024-04-10 12:32:48 +0530

    Add files via upload

---
+++ end_of_file.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class end_of_file {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s=new Scanner(System.in);
+        int lineNumber = 0;
+        while(s.hasNext()){
+            lineNumber++;
+            System.out.println(" "+lineNumber+" "+s.nextLine());
+        
+    }
+    }
+}
